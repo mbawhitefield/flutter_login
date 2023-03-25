468843d135f5ecfd59fdc4f4f0b4a03e16a37e78
mbawhitefield <75375423+mbawhitefield@users.noreply.github.com>
2023-03-25 22:24:42 +0100

    flutter google sign in

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  final user = FirebaseAuth.instance.currentUser!;
+
+  //sign user out method
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            onPressed: signUserOut,
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'LOGGED IN As: ${user.email!}',
+          style: const TextStyle(
+            fontSize: 20,
+          ),
+        ), // or + user.email!
+      ),
+    );
+  }
+}
